21519724dc5d76b360f92bb867094c85857819ba
Costar Mihai <mihai.costar@yahoo.com>
2023-02-18 14:25:10 +0200

    beautify

--- src/elevatorMaintenance.java
+++ src/elevatorMaintenance.java
@@ -2,12 +2,12 @@ import java.util.ArrayList;
 
 public class elevatorMaintenance {
     public static int[] solution(int area) {
-        int sum=area;
-        ArrayList<Integer> arr= new ArrayList();
-        while( sum!=0){
+        int sum = area;
+        ArrayList<Integer> arr = new ArrayList();
+        while (sum != 0) {
             int item = (int) Math.sqrt(sum);
-            arr.add((int) Math.pow(item,2));
-            sum=sum - ((int) Math.pow(item,2));
+            arr.add((int) Math.pow(item, 2));
+            sum = sum - ((int) Math.pow(item, 2));
         }
         return arr.stream().mapToInt(Integer::intValue).toArray();
     }
